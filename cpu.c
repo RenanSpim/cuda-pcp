3e08b4ae17864fc91d48aa9175e96ba785231259
RenanSpim <renan.sinhorini@unesp.br>
2025-11-07 15:16:12 -0300

    oiii

--- cpu.c
+++ cpu.c
@@ -5,29 +5,30 @@
 #define N 10
 #define M 20
 
-int mat[N][M];
+int matI[N][M];
+int matP[N][M];
 
-void heal(int x, int y) {
+void heal(int **mat, int x, int y) {
     int val = rand() % 10000;
 
     if (mat[x][y] == -1)
         mat[x][y] = val < 1000 ? 1 : val < 4000 ? -1 : -2;
 }
 
-void contaminate(int x, int y) {
+void contaminate(int **mat, int x, int y) {
     if (mat[x][y] != 1) return;
 
     if (
-        (x > 0 && mat[x-1][y] < 0)       ||
-        (y > 0 && mat[x][y-1] < 0)       ||
+        (x > 0       && mat[x-1][y] < 0) ||
         (x < (N - 1) && mat[x+1][y] < 0) ||
+        (y > 0       && mat[x][y-1] < 0) ||
         (y < (M - 1) && mat[x][y+1] < 0)
     ) {
         mat[x][y] = -1;
     }
 }
 
-void removeDead(int x, int y) {
+void removeDead(int **mat, int x, int y) {
     if (mat[x][y] == -2)
         mat[x][y] = -3;
 
@@ -35,26 +36,26 @@ void removeDead(int x, int y) {
         mat[x][y] = 0;
 }
 
-void contaminateAll() {
+void contaminateAll(int x) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            contaminate(i, j);
+            contaminate(x % 2 == 0 ? matP : matI, i, j);
         }
     }
 }
 
-void healAll() {
+void healAll(int x) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            heal(i, j);
+            heal(x % 2 == 0 ? matP : matI, i, j);
         }
     }
 }
 
-void removeAllDead() {
+void removeAllDead(int x) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            removeDead(i, j);
+            removeDead(x % 2 == 0 ? matP : matI, i, j);
         }
     }
 }
@@ -63,8 +64,8 @@ int main() {
     srand(time(NULL));
 
     for (int i = 0; i < N*M; i++) {
-        contaminateAll();
-        healAll();
-        removeAllDead();
+        contaminateAll(i);
+        healAll(i);
+        removeAllDead(i);
     }
 }
