059f876cd2dcf8f872e7d04867dfaa8aa70f935b
RenanSpim <renan.sinhorini@unesp.br>
2025-11-22 23:23:03 -0300

    bernal foi embora

--- src/cpu.cu
+++ src/cpu.cu
@@ -4,6 +4,40 @@
 
 #define INDEX(i, j, M) ((i) * (M) + (j))
 
+int N, M, *matI, *matP, qtdT = 0, qtdD = 0;
+
+#define INDEX(i, j, M) ((i) * (M) + (j))
+
+void heal(int *mat, int x, int y) {
+    int val = rand() % 10000;
+
+    if (mat[INDEX(x, y, M)] == -1)
+        mat[INDEX(x, y, M)] = val < 1000 ? 1 : val < 4000 ? -1 : -2;
+}
+
+void contaminate(int *mat, int x, int y) {
+    if (mat[INDEX(x, y, M)] != 1) return;
+
+    if (
+        (x > 0       && mat[(x-1) * M + y] < 0) ||
+        (x < (N - 1) && mat[(x+1) * M + y] < 0) ||
+        (y > 0       && mat[x * M + (y-1)] < 0) ||
+        (y < (M - 1) && mat[x * M + (y+1)] < 0)
+    ) {
+        mat[INDEX(x, y, M)] = -1;
+    }
+}
+
+void removeDead(int *mat, int x, int y) {
+    if (mat[INDEX(x, y, M)] == -2) {
+        mat[INDEX(x, y, M)] = -3;
+        qtdD++;
+    }
+
+    if (mat[INDEX(x, y, M)] == -3)
+        mat[INDEX(x, y, M)] = 0;
+}
+
 void contaminateAll(int x) {
     if (x % 2 == 0) {
         for (int i = 0; i < N; i++) {
